501e681111bd1ce2b0d4d4b69b4a24db836b8b52
matthz-max <matheus7serafim@gmail.com>
2025-08-06 22:03:20 -0300

    pages completas

--- src/data/CorreCertoData.jsx
+++ src/data/CorreCertoData.jsx
@@ -112,4 +112,340 @@ export const CorreData = [
       },
     ],
   },
+  {
+    id: 4,
+    titulo: "Nintendo Switch usado",
+    resumo: "Switch desbloqueado com jogos grátis.",
+    descricaoCompleta:
+      "O Nintendo Switch Desbloqueado na versão Oled Pode Baixar Todos Os Jogos Completos de Graça isso mesmo de Graça! É a melhor escolha para jogadores que procuram uma experiência imersiva e portátil, com tela de alta resolução, recursos avançados e bateria de longa duração.",
+    imagem:
+      "https://i.pinimg.com/736x/72/6d/49/726d490e062376471b3f41466facf922.jpg",
+    preco: "789,50",
+    autor: "João Vendedor",
+    fotoAutor:
+      "https://i.pinimg.com/736x/c4/66/90/c466907bfa75c5598d6a193a589531a4.jpg",
+    data: "17/06/25",
+    hora: "15:30",
+    regiao: "Sudeste",
+    telefone: "11953716330",
+    comentarios: [
+      {
+        nome: "Rihanna Josephine",
+        avatar: "https://i.pravatar.cc/80?img=32",
+        hora: "19h",
+        texto: "Comprei e não me arrependo muito bom",
+      },
+      {
+        nome: "Sophey Paul",
+        avatar: "https://i.pravatar.cc/80?img=12",
+        hora: "10h",
+        texto:
+          "Vendedor muito confiável, entregou na mesma semana. Compraria de novo!",
+      },
+      {
+        nome: "John Selese",
+        avatar: "https://i.pravatar.cc/80?img=7",
+        hora: "10h",
+        texto: "Golpista cuidado",
+      },
+    ],
+  },{
+    id: 5,
+    titulo: "Nintendo Switch usado",
+    resumo: "Switch desbloqueado com jogos grátis.",
+    descricaoCompleta:
+      "O Nintendo Switch Desbloqueado na versão Oled Pode Baixar Todos Os Jogos Completos de Graça isso mesmo de Graça! É a melhor escolha para jogadores que procuram uma experiência imersiva e portátil, com tela de alta resolução, recursos avançados e bateria de longa duração.",
+    imagem:
+      "https://i.pinimg.com/736x/72/6d/49/726d490e062376471b3f41466facf922.jpg",
+    preco: "789,50",
+    autor: "João Vendedor",
+    fotoAutor:
+      "https://i.pinimg.com/736x/c4/66/90/c466907bfa75c5598d6a193a589531a4.jpg",
+    data: "17/06/25",
+    hora: "15:30",
+    regiao: "Sudeste",
+    telefone: "11953716330",
+    comentarios: [
+      {
+        nome: "Rihanna Josephine",
+        avatar: "https://i.pravatar.cc/80?img=32",
+        hora: "19h",
+        texto: "Comprei e não me arrependo muito bom",
+      },
+      {
+        nome: "Sophey Paul",
+        avatar: "https://i.pravatar.cc/80?img=12",
+        hora: "10h",
+        texto:
+          "Vendedor muito confiável, entregou na mesma semana. Compraria de novo!",
+      },
+      {
+        nome: "John Selese",
+        avatar: "https://i.pravatar.cc/80?img=7",
+        hora: "10h",
+        texto: "Golpista cuidado",
+      },
+    ],
+  },{
+    id: 6,
+    titulo: "Nintendo Switch usado",
+    resumo: "Switch desbloqueado com jogos grátis.",
+    descricaoCompleta:
+      "O Nintendo Switch Desbloqueado na versão Oled Pode Baixar Todos Os Jogos Completos de Graça isso mesmo de Graça! É a melhor escolha para jogadores que procuram uma experiência imersiva e portátil, com tela de alta resolução, recursos avançados e bateria de longa duração.",
+    imagem:
+      "https://i.pinimg.com/736x/72/6d/49/726d490e062376471b3f41466facf922.jpg",
+    preco: "789,50",
+    autor: "João Vendedor",
+    fotoAutor:
+      "https://i.pinimg.com/736x/c4/66/90/c466907bfa75c5598d6a193a589531a4.jpg",
+    data: "17/06/25",
+    hora: "15:30",
+    regiao: "Sudeste",
+    telefone: "11953716330",
+    comentarios: [
+      {
+        nome: "Rihanna Josephine",
+        avatar: "https://i.pravatar.cc/80?img=32",
+        hora: "19h",
+        texto: "Comprei e não me arrependo muito bom",
+      },
+      {
+        nome: "Sophey Paul",
+        avatar: "https://i.pravatar.cc/80?img=12",
+        hora: "10h",
+        texto:
+          "Vendedor muito confiável, entregou na mesma semana. Compraria de novo!",
+      },
+      {
+        nome: "John Selese",
+        avatar: "https://i.pravatar.cc/80?img=7",
+        hora: "10h",
+        texto: "Golpista cuidado",
+      },
+    ],
+  },{
+    id: 7,
+    titulo: "Nintendo Switch usado",
+    resumo: "Switch desbloqueado com jogos grátis.",
+    descricaoCompleta:
+      "O Nintendo Switch Desbloqueado na versão Oled Pode Baixar Todos Os Jogos Completos de Graça isso mesmo de Graça! É a melhor escolha para jogadores que procuram uma experiência imersiva e portátil, com tela de alta resolução, recursos avançados e bateria de longa duração.",
+    imagem:
+      "https://i.pinimg.com/736x/72/6d/49/726d490e062376471b3f41466facf922.jpg",
+    preco: "789,50",
+    autor: "João Vendedor",
+    fotoAutor:
+      "https://i.pinimg.com/736x/c4/66/90/c466907bfa75c5598d6a193a589531a4.jpg",
+    data: "17/06/25",
+    hora: "15:30",
+    regiao: "Sudeste",
+    telefone: "11953716330",
+    comentarios: [
+      {
+        nome: "Rihanna Josephine",
+        avatar: "https://i.pravatar.cc/80?img=32",
+        hora: "19h",
+        texto: "Comprei e não me arrependo muito bom",
+      },
+      {
+        nome: "Sophey Paul",
+        avatar: "https://i.pravatar.cc/80?img=12",
+        hora: "10h",
+        texto:
+          "Vendedor muito confiável, entregou na mesma semana. Compraria de novo!",
+      },
+      {
+        nome: "John Selese",
+        avatar: "https://i.pravatar.cc/80?img=7",
+        hora: "10h",
+        texto: "Golpista cuidado",
+      },
+    ],
+  },{
+    id: 8,
+    titulo: "Nintendo Switch usado",
+    resumo: "Switch desbloqueado com jogos grátis.",
+    descricaoCompleta:
+      "O Nintendo Switch Desbloqueado na versão Oled Pode Baixar Todos Os Jogos Completos de Graça isso mesmo de Graça! É a melhor escolha para jogadores que procuram uma experiência imersiva e portátil, com tela de alta resolução, recursos avançados e bateria de longa duração.",
+    imagem:
+      "https://i.pinimg.com/736x/72/6d/49/726d490e062376471b3f41466facf922.jpg",
+    preco: "789,50",
+    autor: "João Vendedor",
+    fotoAutor:
+      "https://i.pinimg.com/736x/c4/66/90/c466907bfa75c5598d6a193a589531a4.jpg",
+    data: "17/06/25",
+    hora: "15:30",
+    regiao: "Sudeste",
+    telefone: "11953716330",
+    comentarios: [
+      {
+        nome: "Rihanna Josephine",
+        avatar: "https://i.pravatar.cc/80?img=32",
+        hora: "19h",
+        texto: "Comprei e não me arrependo muito bom",
+      },
+      {
+        nome: "Sophey Paul",
+        avatar: "https://i.pravatar.cc/80?img=12",
+        hora: "10h",
+        texto:
+          "Vendedor muito confiável, entregou na mesma semana. Compraria de novo!",
+      },
+      {
+        nome: "John Selese",
+        avatar: "https://i.pravatar.cc/80?img=7",
+        hora: "10h",
+        texto: "Golpista cuidado",
+      },
+    ],
+  },{
+    id: 9,
+    titulo: "Nintendo Switch usado",
+    resumo: "Switch desbloqueado com jogos grátis.",
+    descricaoCompleta:
+      "O Nintendo Switch Desbloqueado na versão Oled Pode Baixar Todos Os Jogos Completos de Graça isso mesmo de Graça! É a melhor escolha para jogadores que procuram uma experiência imersiva e portátil, com tela de alta resolução, recursos avançados e bateria de longa duração.",
+    imagem:
+      "https://i.pinimg.com/736x/72/6d/49/726d490e062376471b3f41466facf922.jpg",
+    preco: "789,50",
+    autor: "João Vendedor",
+    fotoAutor:
+      "https://i.pinimg.com/736x/c4/66/90/c466907bfa75c5598d6a193a589531a4.jpg",
+    data: "17/06/25",
+    hora: "15:30",
+    regiao: "Sudeste",
+    telefone: "11953716330",
+    comentarios: [
+      {
+        nome: "Rihanna Josephine",
+        avatar: "https://i.pravatar.cc/80?img=32",
+        hora: "19h",
+        texto: "Comprei e não me arrependo muito bom",
+      },
+      {
+        nome: "Sophey Paul",
+        avatar: "https://i.pravatar.cc/80?img=12",
+        hora: "10h",
+        texto:
+          "Vendedor muito confiável, entregou na mesma semana. Compraria de novo!",
+      },
+      {
+        nome: "John Selese",
+        avatar: "https://i.pravatar.cc/80?img=7",
+        hora: "10h",
+        texto: "Golpista cuidado",
+      },
+    ],
+  },{
+    id: 10,
+    titulo: "Nintendo Switch usado",
+    resumo: "Switch desbloqueado com jogos grátis.",
+    descricaoCompleta:
+      "O Nintendo Switch Desbloqueado na versão Oled Pode Baixar Todos Os Jogos Completos de Graça isso mesmo de Graça! É a melhor escolha para jogadores que procuram uma experiência imersiva e portátil, com tela de alta resolução, recursos avançados e bateria de longa duração.",
+    imagem:
+      "https://i.pinimg.com/736x/72/6d/49/726d490e062376471b3f41466facf922.jpg",
+    preco: "789,50",
+    autor: "João Vendedor",
+    fotoAutor:
+      "https://i.pinimg.com/736x/c4/66/90/c466907bfa75c5598d6a193a589531a4.jpg",
+    data: "17/06/25",
+    hora: "15:30",
+    regiao: "Sudeste",
+    telefone: "11953716330",
+    comentarios: [
+      {
+        nome: "Rihanna Josephine",
+        avatar: "https://i.pravatar.cc/80?img=32",
+        hora: "19h",
+        texto: "Comprei e não me arrependo muito bom",
+      },
+      {
+        nome: "Sophey Paul",
+        avatar: "https://i.pravatar.cc/80?img=12",
+        hora: "10h",
+        texto:
+          "Vendedor muito confiável, entregou na mesma semana. Compraria de novo!",
+      },
+      {
+        nome: "John Selese",
+        avatar: "https://i.pravatar.cc/80?img=7",
+        hora: "10h",
+        texto: "Golpista cuidado",
+      },
+    ],
+  },
+  {
+    id: 11,
+    titulo: "Nintendo Switch usado",
+    resumo: "Switch desbloqueado com jogos grátis.",
+    descricaoCompleta:
+      "O Nintendo Switch Desbloqueado na versão Oled Pode Baixar Todos Os Jogos Completos de Graça isso mesmo de Graça! É a melhor escolha para jogadores que procuram uma experiência imersiva e portátil, com tela de alta resolução, recursos avançados e bateria de longa duração.",
+    imagem:
+      "https://i.pinimg.com/736x/72/6d/49/726d490e062376471b3f41466facf922.jpg",
+    preco: "789,50",
+    autor: "João Vendedor",
+    fotoAutor:
+      "https://i.pinimg.com/736x/c4/66/90/c466907bfa75c5598d6a193a589531a4.jpg",
+    data: "17/06/25",
+    hora: "15:30",
+    regiao: "Sudeste",
+    telefone: "11953716330",
+    comentarios: [
+      {
+        nome: "Rihanna Josephine",
+        avatar: "https://i.pravatar.cc/80?img=32",
+        hora: "19h",
+        texto: "Comprei e não me arrependo muito bom",
+      },
+      {
+        nome: "Sophey Paul",
+        avatar: "https://i.pravatar.cc/80?img=12",
+        hora: "10h",
+        texto:
+          "Vendedor muito confiável, entregou na mesma semana. Compraria de novo!",
+      },
+      {
+        nome: "John Selese",
+        avatar: "https://i.pravatar.cc/80?img=7",
+        hora: "10h",
+        texto: "Golpista cuidado",
+      },
+    ],
+  },
+  {
+    id: 12,
+    titulo: "Nintendo Switch usado",
+    resumo: "Switch desbloqueado com jogos grátis.",
+    descricaoCompleta:
+      "O Nintendo Switch Desbloqueado na versão Oled Pode Baixar Todos Os Jogos Completos de Graça isso mesmo de Graça! É a melhor escolha para jogadores que procuram uma experiência imersiva e portátil, com tela de alta resolução, recursos avançados e bateria de longa duração.",
+    imagem:
+      "https://i.pinimg.com/736x/72/6d/49/726d490e062376471b3f41466facf922.jpg",
+    preco: "789,50",
+    autor: "João Vendedor",
+    fotoAutor:
+      "https://i.pinimg.com/736x/c4/66/90/c466907bfa75c5598d6a193a589531a4.jpg",
+    data: "17/06/25",
+    hora: "15:30",
+    regiao: "Sudeste",
+    telefone: "11953716330",
+    comentarios: [
+      {
+        nome: "Rihanna Josephine",
+        avatar: "https://i.pravatar.cc/80?img=32",
+        hora: "19h",
+        texto: "Comprei e não me arrependo muito bom",
+      },
+      {
+        nome: "Sophey Paul",
+        avatar: "https://i.pravatar.cc/80?img=12",
+        hora: "10h",
+        texto:
+          "Vendedor muito confiável, entregou na mesma semana. Compraria de novo!",
+      },
+      {
+        nome: "John Selese",
+        avatar: "https://i.pravatar.cc/80?img=7",
+        hora: "10h",
+        texto: "Golpista cuidado",
+      },
+    ],
+  },
 ];
--- src/pages/login/Register.jsx
+++ src/pages/login/Register.jsx
@@ -1,44 +1,65 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Periferia from "../../assets/images/periferia.png";
 
 export default function Register() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex items-center w-full h-screen justify-center">
-      <div className="bg-white rounded-xl p-8 border border-black-100 shadow-lg w-full max-w-[60vw]">
-        <h2 className="text-3xl font-bold text-center mb-2">Cadastro</h2>
-        <p className="text-center text-gray-500 mb-6">Crie sua conta</p>
+    <div className="flex flex-col lg:flex-row items-center justify-center w-full h-screen gap-x-6 overflow-hidden">
+      {/* Formulário */}
+      <div className="w-full lg:w-1/2 flex justify-center items-center px-4">
+        <div className="bg-white rounded-2xl p-12 border border-gray-200 shadow-xl w-full max-w-2xl">
+          <h2 className="text-4xl font-bold text-center mb-4">Cadastro</h2>
+          <p className="text-center text-gray-500 mb-8 text-lg">
+            Crie sua conta
+          </p>
 
-        <input
-          type="text"
-          placeholder="Nome"
-          className="w-full mb-4 px-4 py-2 border rounded-md"
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          className="w-full mb-4 px-4 py-2 border rounded-md"
-        />
-        <input
-          type="password"
-          placeholder="Senha"
-          className="w-full mb-4 px-4 py-2 border rounded-md"
-        />
+          <input
+            type="text"
+            placeholder="Nome"
+            className="w-full mb-5 px-5 py-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 text-lg"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            className="w-full mb-5 px-5 py-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 text-lg"
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            className="w-full mb-6 px-5 py-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 text-lg"
+          />
 
-        <button className="w-full bg-green-600 text-white py-2 rounded-md trasition duration-300 hover:bg-green-700 mb-4 hover:scale-105">
-          Cadastrar
-        </button>
-
-        <p className="text-center text-sm">
-          Já tem uma conta?{" "}
-          <button
-            onClick={() => navigate("/")}
-            className="text-blue-600 trasition-300 hover:underline "
-          >
-            Faça login
+          <button className="w-full bg-green-500 text-white py-3 text-lg rounded-md transition duration-300 hover:bg-green-600 hover:scale-105">
+            Cadastrar
           </button>
-        </p>
+
+          <p className="text-center text-sm mt-6">
+            Já tem uma conta?{" "}
+            <button
+              onClick={() => navigate("/")}
+              className="text-blue-600 transition duration-300 hover:underline"
+            >
+              Faça login
+            </button>
+          </p>
+        </div>
+      </div>
+
+      {/* Imagem e texto */}
+      <div className="hidden lg:block w-1/2 h-screen relative">
+        <img
+          src={Periferia}
+          alt="fundo"
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center bg-black bg-opacity-60">
+          <h1 className="text-4xl font-bold text-orange-500 mb-2">
+            BlogPeriférico
+          </h1>
+          <p className="text-lg">Bem-vindo ao blog periférico!</p>
+        </div>
       </div>
     </div>
   );
